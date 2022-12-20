11d3bab1db5be47e099aab8e1d21e567c7e6ca2b
Guilherme Maciel de Melo <gmm7@WINDOWS.CIN.UFPE.BR>
2022-12-20 14:15:32 -0300

    Arquivo com as informações das matrizes

--- a_estrela.py
+++ a_estrela.py
@@ -4,12 +4,15 @@ class algoritmo_estrela:
 		self.matrix_dist = matrix_distancia
 		self.border_list = lista_fronteira
 		self.h_list = lista_h
-        self.links_list = lista_relacionamentos
+		self.links_list = lista_relacionamentos
 		self.node = 0
-    def select(self):
-	    self.node = lista_h.index(min(lista_h))
-    def test(self, destino):
-	    return self.border_list[self.node] == destino
-    def verify(self):
-    def add_border(self):
+
+	def select(self):
+		self.node = self.lista_h.index(min(self.lista_h))
+	def test(self, destino):
+		return self.border_list[self.node] == destino
+	def verify(self):
+		pass
+	def add_border(self):
+		pass
 
